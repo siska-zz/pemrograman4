ea0a58d5fedeffe76133c5e52453431b92927539
siska <chika_puff1@yahoo.co.id>
2011-09-30 15:13:06 +0700

    classvsobject

---
+++ sesi2/src/constructor/person.java
@@ -0,0 +1,11 @@
+public class person{
+
+       public person(){
+	   }
+	   public person(String nama){
+	          System.out.println(nama);
+	   }
+	   public person(String nama, int l){
+	          System.out.println(nama + l);
+	   }
+}
---
+++ sesi2/src/method/person.java
@@ -0,0 +1,13 @@
+public class person{
+       public void IniMethod(){
+	   
+	   }
+	   public String getName(){
+	          return "dani";
+	   }
+	   public void methodParam(String nama){
+	   }
+	   private int tambah(int a, int b){
+	           return a+b;
+	   }
+}
